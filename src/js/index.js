70773a11905a43e5fec65254e39bba67e7f51f37
Yug-soni <abc@example.com>
2020-09-12 21:40:50 +0530

    Trying the new features.

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,17 @@ elements.form.addEventListener('submit', e => {
 
 elements.button.addEventListener('click', () => {
     const value = elements.message.value;
+    fetchText();
     console.log(value);
     message.value = '';
 });
+
+const fetchText = async () => {
+    const response = await fetch('http://localhost:9999/',{ mode: 'no-cors' });
+    console.log(response.status);
+    console.log(response.statusText);
+
+    response.json().then(data => {console.log(data)});
+
+    console.log(await response.text());
+};
