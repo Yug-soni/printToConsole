a0160b429ae9b5e252ba1bd32b57a680d58bea82
Yug-soni <abc@example.com>
2020-09-12 14:11:14 +0530

    Server added and functionality is working.

---
+++ src/js/server/app.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const { over } = require('lodash');
+
+const server = http.createServer( (req, res) => {
+    const pathname = url.parse(req.url, true).pathname;
+    const urlget = url.parse(req.url, true);
+
+    if(pathname === '/') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        fs.readFile(`${__dirname}/../../index.html`, 'utf-8', (err, data) => {
+            res.end(data);
+        });
+    } else if (pathname === '/add-message') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+    }else {
+        res.writeHead(404, {'Content-type': 'text/html'});
+        res.end("No data");
+    }
+
+});
+
+server.listen(9999, '127.0.0.1', () => {
+    console.log("Server started.");
+});
