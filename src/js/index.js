582e786ea011629e7ad91682df150cc4b8a4cc05
Yug-soni <abc@example.com>
2020-09-18 19:02:57 +0530

    bug fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
-import {elements} from './views/base';
+import {elements} from './views/base'
 
-/*
+
 elements.form.addEventListener('submit', e => {
     e.preventDefault();
 });
-*/
+
 
 elements.button.addEventListener('click', () => {
     const value = elements.message.value;
